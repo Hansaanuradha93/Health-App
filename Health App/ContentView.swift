439acfde1401126b85ca5e0fd0e41f0130e4ed4e
Hansa Wickramanayake <hansaanuradha93@gmail.com>
2024-09-04 21:29:43 +0530

    Initial Commit

---
+++ Health App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Health App
+//
+//  Created by Hansa Wickramanayake on 2024-09-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Health App/Health_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_AppApp.swift
+//  Health App
+//
+//  Created by Hansa Wickramanayake on 2024-09-04.
+//
+
+import SwiftUI
+
+@main
+struct Health_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
