a76670767ec1d8033c70a6595b01d7ef9f4f859a
Hansa Wickramanayake <hansaanuradha93@gmail.com>
2024-10-07 13:46:06 +0530

    fetch steps count

--- Health App/Screens/Dashbard View/DashboardViewModel.swift
+++ Health App/Screens/Dashbard View/DashboardViewModel.swift
@@ -14,4 +14,24 @@ enum DashboardSelectedTab {
 
 final class DashboardViewModel: ObservableObject {
     @Published var selectedTab: DashboardSelectedTab = .steps
+    @Published var dailyStepCounts: [Date: Double] = [:]
+    
+    func fetchSteps() {
+        let stepsQuery = HealthKitManager.shared.getStepsQuery()
+        
+        HealthKitManager.shared.fetchHealthData(with: stepsQuery) { dailyStepCounts, error in
+            if let error {
+                printError(error)
+                return
+            }
+            
+            guard let dailyStepCounts else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.dailyStepCounts = dailyStepCounts
+            }
+        }
+    }
 }
